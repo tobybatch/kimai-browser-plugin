9073bcc9e7cf3482084ae8faa00b68e66f70459c
Toby Batch <tobias@neontribe.co.uk>
2021-05-07 09:27:47 +0100

    feat: fixed and hard coded neon kimai

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import {getResourceURL} from "./services/assets-service";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 // import * as AssetsService from "./services/assets-service";
 
-function KimaiIframe() {
-    let iframe = <div>Loading...</div>
-    useEffect(() => {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, tabs => {
-            const currentUrl = tabs[0].url || ""
-            const url = "http://home.batch.org.uk:9001/en/?u=" + encodeURI(currentUrl)
-            iframe = <iframe
-                title="kimai-iframe"
-                width=
-
-        });
-    }, []);
-
-    return null;
-}
-
 function App() {
+
+    const [kimaiUrl, setKimaiUrl] = useState( '' );
+
+        useEffect(() => {
+            chrome.tabs.query({
+                active: true,
+                currentWindow: true
+            }, tabs => {
+                const currentUrl = tabs[0].url || ""
+                setKimaiUrl("https://kimai.neontribe.org/en/timesheet/create?source=" + encodeURI(currentUrl))
+            });
+        }, []);
+
     return (
         <div className="App">
-            <KimaiIframe/>
+            <iframe
+            title="kimai-iframe"
+            width="750"
+            height="580"
+            src={kimaiUrl}
+        />
         </div>
     );
 }
