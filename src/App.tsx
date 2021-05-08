16f890a9a53c70da83d7f05834992b01370173be
Toby Batch <tobias@neontribe.co.uk>
2021-05-08 11:17:36 +0100

    feat: made app server stand alone

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import {getResourceURL} from "./services/assets-service";
 import {useEffect, useState} from "react";
 
 // import * as AssetsService from "./services/assets-service";
 
 function App() {
 
-    const [kimaiUrl, setKimaiUrl] = useState( '' );
+    const [kimaiUrl, setKimaiUrl] = useState('');
 
-        useEffect(() => {
+    useEffect(() => {
+        if (chrome.tabs !== undefined) {
             chrome.tabs.query({
                 active: true,
                 currentWindow: true
             }, tabs => {
                 const currentUrl = tabs[0].url || ""
-                setKimaiUrl("https://kimai.neontribe.org/en/timesheet/create?source=" + encodeURI(currentUrl))
+                setKimaiUrl(getKimaiUrl(currentUrl))
             });
-        }, []);
+        } else {
+            setKimaiUrl(getKimaiUrl("https://github.com/tobybatch/kimai2/issues/247"))
+        }
+    }, []);
+
+    const getKimaiUrl = (currentUrl) => {
+        return "https://kimai.neontribe.org/en/timesheet/create?source=" + encodeURI(currentUrl);
+    }
 
     return (
         <div className="App">
             <iframe
-            title="kimai-iframe"
-            width="750"
-            height="580"
-            src={kimaiUrl}
-        />
+                title="kimai-iframe"
+                width="750"
+                height="580"
+                src={kimaiUrl}
+            />
         </div>
     );
 }
