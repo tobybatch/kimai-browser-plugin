4e9da75eea7561f2fee827d66061ecc468389c03
Toby Batch <tobias@neontribe.co.uk>
2021-07-05 12:43:02 +0100

    fix: inline the date/duration fields

--- BrowserPluginBundle/Listener/ResponseListener.php
+++ BrowserPluginBundle/Listener/ResponseListener.php
@@ -30,9 +30,11 @@ class ResponseListener
         if ($request->get("kimaiBrowserPlugin") && str_ends_with($request->getPathInfo(), "timesheet/create")) {
             $response = $event->getResponse();
             $content = $response->getContent();
-            $css = "<style>.box-header.with-border, .toolbar-pad.no-print, .content-header, header { display: none; } .content-wrapper { padding-top: 0px !important; }</style>";
+            $css = "<style>.box-header.with-border, .toolbar-pad.no-print, .content-header, header { display: none; } .content-wrapper { padding-top: 0px !important; } .row:nth-child(1) { display: inline-flex !important; }</style>";
             $newContent = substr_replace($content, $css, strpos($content, "</head>"), 0);
             $response->setContent($newContent);
         }
     }
 }
+// display: inline-flex;
+
